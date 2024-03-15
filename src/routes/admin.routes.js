b72cec9f0f4ac3cf7729e1f371daf0f795f94679
DDarioBenitez <reedq2.alt@gmail.com>
2024-03-15 02:17:10 -0300

    Crud

---
+++ src/routes/admin.routes.js
@@ -0,0 +1,21 @@
+// ************ Require's ************
+const express = require('express');
+const router = express.Router();
+
+// ************ Controller Require ************
+const { create, store, edit, update, destroy } = require("../controllers/admin");
+const { uploadProducts } = require('../middlewares/uploadFiles');
+
+// desde app.js viene con /admin
+/*** CREATE ONE PRODUCT ***/
+router.get('/crear-producto', create);
+router.post('/crear-producto', uploadProducts.single('img'), store);
+
+/*** EDIT ONE PRODUCT ***/
+router.get('/editar-producto/:id', edit);
+router.put('/editar-producto/:id', uploadProducts.single('img'), update);
+
+/*** DELETE ONE PRODUCT***/
+router.delete('/eliminar-producto/:id', destroy);
+
+module.exports = router;
